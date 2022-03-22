b13e56c6a7569625d26f3ed976026fb3b275b1d1
VictordeLara <victorlara2010@gmail.com>
2022-03-22 01:53:45 -0300

    Click

--- app/src/main/java/com/example/recyclerview/activity/MainActivity.java
+++ app/src/main/java/com/example/recyclerview/activity/MainActivity.java
@@ -6,9 +6,13 @@ import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.LinearLayout;
+import android.widget.Toast;
 
 import com.example.recyclerview.R;
+import com.example.recyclerview.RecyclerItemClickListener;
 import com.example.recyclerview.adapter.Adapter;
 import com.example.recyclerview.model.Filme;
 
@@ -40,6 +44,32 @@ public class MainActivity extends AppCompatActivity {
         recyclerView.setHasFixedSize(true);
         recyclerView.addItemDecoration( new DividerItemDecoration(this, LinearLayout.VERTICAL));
         recyclerView.setAdapter(adapter);
+
+        //click
+        recyclerView.addOnItemTouchListener(
+               new RecyclerItemClickListener(
+                       getApplicationContext(),
+                       recyclerView,
+                       new RecyclerItemClickListener.OnItemClickListener() {
+                           @Override
+                           public void onItemClick(View view, int position) {
+                               Filme filme = listaFilmes.get( position );
+                               Toast.makeText(getApplicationContext(), "Item pressionado: " + filme.getTituloFilme(), Toast.LENGTH_SHORT).show();
+                           }
+
+                           @Override
+                           public void onLongItemClick(View view, int position) {
+                               Filme filme = listaFilmes.get( position );
+                               Toast.makeText(getApplicationContext(), "Click long: " + filme.getTituloFilme(), Toast.LENGTH_SHORT).show();
+                           }
+
+                           @Override
+                           public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+
+                           }
+                       }
+               )
+        );
 
     }
 
